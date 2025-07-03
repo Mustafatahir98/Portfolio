03c221f5332f4cf9159203de5b3e8eb1e9cd2234
Mustafa Tahir <mustafatahir14.mt@gmail.com>
2025-07-03 14:35:00 +0500

    Update Work.jsx

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,7 +13,7 @@ import proj14 from '../assets/proj14.jpg';
 import proj15 from '../assets/proj15.jpg';
 
 const Work = () => {
-  const [visibleProjects, setVisibleProjects] = useState(4); // Initially show 4 projects
+  const [visibleProjects, setVisibleProjects] = useState(4);
   const projects = [
     {
       img: proj1,
@@ -22,6 +22,7 @@ const Work = () => {
       styling: "Tailwind CSS",
       tools: "Visual Studio Code",
       description: "A VR frontend application built with React JS for immersive experiences.",
+      link: ""
     },
     {
       img: proj3,
@@ -30,6 +31,7 @@ const Work = () => {
       styling: "Tailwind CSS",
       tools: "Visual Studio Code, Postman",
       description: "A complete Medicare booking application integrated with Stripe for payments.",
+      link: ""
     },
     {
       img: proj5,
@@ -38,6 +40,7 @@ const Work = () => {
       styling: "CSS3",
       tools: "Visual Studio Code",
       description: "An educational website built with React JS and Web3Forms for handling user submissions.",
+      link: ""
     },
     {
       img: proj6,
@@ -46,6 +49,7 @@ const Work = () => {
       styling: "CSS3",
       tools: "Visual Studio Code",
       description: "A Netflix clone using Firebase for authentication and TMDB API for movie data.",
+      link: ""
     },
     {
       img: proj8,
@@ -54,6 +58,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A toll and gas calculator to estimate costs for road trips using TollGuru and Mapbox APIs.",
+      link: ""
     },
     {
       img: proj9,
@@ -62,6 +67,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A tool to extract and analyze entities from text using TextRazor and Google Search API.",
+      link: ""
     },
     {
       img: proj10,
@@ -70,6 +76,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A performance reporting system that integrates PageSpeed API for reporting.",
+      link: ""
     },
     {
       img: proj11,
@@ -78,6 +85,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A tool for generating image metadata using the OpenAI API and other Python libraries.",
+      link: ""
     },
     {
       img: proj12,
@@ -86,6 +94,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A search tool for finding nearby Amtrak stations with easy access to station details.",
+      link: ""
     },
     {
       img: proj13,
@@ -94,6 +103,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A tool that collects and displays device information for visitors on the site.",
+      link: ""
     },
     {
       img: proj14,
@@ -102,6 +112,7 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "A content scraping tool that gathers meta-information from web pages for analysis.",
+      link: ""
     },
     {
       img: proj15,
@@ -110,47 +121,90 @@ const Work = () => {
       styling: "CSS & Bootstrap",
       tools: "Visual Studio Code",
       description: "An integration tool that sends SMS messages upon form submission using SalesMessage API.",
+      link: ""
     },
+    {
+      title: "Personal Drivers",
+      link: "https://www.personaldrivers.com/"
+    },
+    {
+      title: "Lotus Psychiatry and Wellness",
+      link: "https://lotuspsychiatryandwellness.com/"
+    },
+    {
+      title: "Soho Tech Services",
+      link: "https://sohotechservices.com/"
+    },
+    {
+      title: "Soho Ad Labs",
+      link: "https://sohoadlabs.com/"
+    },
+    {
+      title: "Lotus Laboratories",
+      link: "https://lotus-laboratories.com/"
+    },
+    {
+      title: "Marovis",
+      link: "https://marovis.com/"
+    },
+    {
+      title: "Professional Drivers",
+      link: "https://www.professionaldrivers.com/"
+    }
   ];
 
   const loadMoreProjects = () => {
-    setVisibleProjects(prev => prev + 4); // Increase the number of visible projects by 4
+    setVisibleProjects(prev => prev + 4);
   };
 
   return (
     <div className='max-w-[1200px] mx-auto p-5' id='work'>
       <div className='pb-8 text-center'>
         <h2 className='primary-color text-4xl mb-3 font-bold'>Work</h2>
-        <p className='text-white sm:text-lg lg:text-xl'>
-          Check out some of my recent work
-        </p>
+        <p className='text-white sm:text-lg lg:text-xl'>Check out some of my recent work</p>
         <div className='mt-4'>
-          <p className='text-orange-600 font-bold text-lg'>
-            “Hover to get details about work”
-          </p>
+          <p className='text-orange-600 font-bold text-lg'>“Hover to get details about work”</p>
         </div>
       </div>
 
       <div className='grid sm:grid-cols-1 md:grid-cols-2 gap-6'>
         {projects.slice(0, visibleProjects).map((project, index) => (
           <div key={index} className="transform transition-transform duration-300 hover:scale-105 overflow-hidden shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div h-[250px] sm:h-[300px] md:h-[400px] bg-cover relative">
-            <img src={project.img} alt="Project Screenshot" className="w-full h-full object-cover" />
+            {project.img ? (
+              <img src={project.img} alt="Project Screenshot" className="w-full h-full object-cover" />
+            ) : (
+              <div className='bg-gray-900 w-full h-full flex items-center justify-center text-white text-xl font-semibold'>
+                {project.title}
+              </div>
+            )}
             <div className="opacity-0 group-hover:opacity-100 bg-black/90 absolute inset-0 flex flex-col justify-center items-center duration-500 ease-in-out transform translate-y-full translate-x-full group-hover:translate-y-0 group-hover:translate-x-0 p-4 md:px-6 shadow-lg m-2 md:m-6 rounded-md">
               <span className="text-lg md:text-2xl font-bold primary-color tracking-wider text-center text-white">{project.title}</span>
-              <ul className="text-sm md:text-base lg:text-lg list-none mt-1 md:mt-4">
-                <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
-                  <span className='mr-2 font-bold text-orange-600'>Frameworks & Libraries:</span>{project.frameworks}
-                </li>
-                <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
-                  <span className='mr-2 font-bold text-orange-600'>CSS Styling:</span>{project.styling}
-                </li>
-                <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
-                  <span className='mr-2 font-bold text-orange-600'>Software & Tools:</span>{project.tools}
-                </li>
-                <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
-                  <span className='mr-2 font-bold text-orange-600'>Description:</span>{project.description}
-                </li>
-              </ul>
+              {project.description && (
+                <ul className="text-sm md:text-base lg:text-lg list-none mt-1 md:mt-4">
+                  <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
+                    <span className='mr-2 font-bold text-orange-600'>Frameworks & Libraries:</span>{project.frameworks}
+                  </li>
+                  <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
+                    <span className='mr-2 font-bold text-orange-600'>CSS Styling:</span>{project.styling}
+                  </li>
+                  <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
+                    <span className='mr-2 font-bold text-orange-600'>Software & Tools:</span>{project.tools}
+                  </li>
+                  <li className="mb-2 text-white pl-6 md:pl-8 relative before:content-['▹'] before:absolute before:left-0 before:text-orange-600 before:text-[24px] md:before:text-[36px]">
+                    <span className='mr-2 font-bold text-orange-600'>Description:</span>{project.description}
+                  </li>
+                </ul>
+              )}
+              {project.link && (
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="mt-4 px-4 py-2 bg-orange-600 text-white rounded-md hover:bg-orange-700"
+                >
+                  Visit Site
+                </a>
+              )}
             </div>
           </div>
         ))}
@@ -165,6 +219,6 @@ const Work = () => {
       )}
     </div>
   );
-}
+};
 
 export default Work;
